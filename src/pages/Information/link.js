c785489ad6fd7f294de044b1ee14d5cf0a44157c
MarceloCFSF <marcelo.cfsf@gmail.com>
2020-10-14 11:06:15 -0300

    Add Links

---
+++ src/pages/Information/link.js
@@ -0,0 +1,28 @@
+const links = {
+  Drive: {
+    Geral: "https://drive.google.com/drive/folders/0B5PPBA8AoawgcjNkc09KQmlSSWM?usp=sharing",
+    Administração: "https://drive.google.com/drive/folders/0B5PPBA8AoawgVGNScmVjakwyTUk?usp=sharing",
+    Aerodinâmica: "https://drive.google.com/drive/folders/0B5PPBA8AoawgdVFncmR6c3JRZ1U?usp=sharing",
+    Comercial: "https://drive.google.com/drive/folders/1yjlCGW8e2rb4-C-JmJahZ-5ZpOjObyuh?usp=sharing",
+    Eletrônica: "https://drive.google.com/drive/folders/1x1gRP0pjikaYv0ehLmee5McpKcXXCwv_?usp=sharing",
+    Estrutura: "https://drive.google.com/drive/folders/0B5PPBA8AoawgajlnNWRhd0NXTEk?usp=sharing",
+    Freios: "https://drive.google.com/drive/folders/0B5PPBA8AoawgSUlBM1lBZlV2cXc?usp=sharing",
+    Marketing: "https://drive.google.com/drive/folders/0B4ffStv_ycJNcERTM2xmM2NsVnM?usp=sharing",
+    Suspensão: "https://drive.google.com/drive/folders/0B3553RAx3CH5Y1dRVUlhc2tKckE?usp=sharing",
+    Powertrain: "https://drive.google.com/drive/folders/0B5PPBA8AoawgellKeFdGSG5EZ3c?usp=sharing",
+  },
+  Teams: {
+    Geral: "https://teams.microsoft.com/l/channel/19%3a5b176386f8c748e9b7915b2091cad0dc%40thread.skype/General?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Administração: "https://teams.microsoft.com/l/team/19%3a551c7969e3b84096ac8b9657260d236a%40thread.skype/conversations?groupId=2c17995a-e48a-4925-a1bb-ab8b0993bd96&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Aerodinâmica: "https://teams.microsoft.com/l/channel/19%3a2e83fa1b52064df095d77ff402ab61ca%40thread.skype/2%2520-%2520Aerodin%25C3%25A2mica?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Comercial: "https://teams.microsoft.com/l/team/19%3a551c7969e3b84096ac8b9657260d236a%40thread.skype/conversations?groupId=2c17995a-e48a-4925-a1bb-ab8b0993bd96&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Eletrônica: "https://teams.microsoft.com/l/channel/19%3af359cd9ffb1b4d2881a87c252daffaeb%40thread.skype/7%2520-%2520Eletr%25C3%25B4nica?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Estrutura: "https://teams.microsoft.com/l/channel/19%3a6b42b77859e3488185925d2520121ce5%40thread.skype/6%2520-%2520Estrutura?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Freios: "https://teams.microsoft.com/l/channel/19%3afb9b31341b8248bc9349321a1742f3ae%40thread.skype/4%2520-%2520Freios?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Marketing: "https://teams.microsoft.com/l/team/19%3a551c7969e3b84096ac8b9657260d236a%40thread.skype/conversations?groupId=2c17995a-e48a-4925-a1bb-ab8b0993bd96&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Suspensão: "https://teams.microsoft.com/l/channel/19%3a05b85df01d874e96ba67dda035971093%40thread.skype/3%2520-%2520Suspens%25C3%25A3o%2520e%2520Dire%25C3%25A7%25C3%25A3o?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+    Powertrain: "https://teams.microsoft.com/l/channel/19%3a1f84ece1ce9e47629f8428ea55aedb21%40thread.skype/5%2520-%2520PowerTrain?groupId=beb01133-47b1-4c48-9a21-3dcd7f8c5aef&tenantId=168d4137-d6f6-45f8-aaa7-d1a70233095e",
+  },
+}
+
+export default links;
